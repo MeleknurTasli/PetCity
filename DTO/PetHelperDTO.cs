857eadd99c2dd467040fbd2f59eaaf0173297d99
Mustafa <mustafanck3@gmail.com>
2022-02-18 14:20:10 +0300

    pethelpercontroller added

--- DTO/PetHelperDTO.cs
+++ DTO/PetHelperDTO.cs
@@ -1,9 +1,7 @@
 public class PetHelperDTO{
 
-    public PetHelperDTO()
-    {
-        
-    }
+  
+    public static List<PetHelperDTO> petHelperStatic = new List<PetHelperDTO>();
     public int Id { get; set; }
     public int UserId { get; set; }
     public string Image { get; set; }
@@ -11,6 +9,9 @@ public class PetHelperDTO{
     public string Latitude { get; set; }
     public string Longtitude { get; set; }
     public string Description { get; set; }
-
+      public PetHelperDTO()
+    {
+        
+    }
 
 }
