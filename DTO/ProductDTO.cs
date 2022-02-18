2895661cc6749c00afd59aa86f011eacf902280c
pirematur3 <ozkean.93@gmail.com>
2022-02-18 12:57:16 +0300

    ProductDTO changed

--- DTO/ProductDTO.cs
+++ DTO/ProductDTO.cs
@@ -1,3 +1,20 @@
 public class ProductDTO{
-    
+
+    public static List<ProductDTO> staticList = new List<ProductDTO>();
+
+    public int ID {get;set;}
+    public string Name { get; set; }
+    public string Detail { get; set; }
+    public string Price { get; set; }
+    public string Stock { get; set; }
+    public string Supplier { get; set; }
+    public string Brand { get; set; }
+    public string Category { get; set; }
+
+    public ProductDTO(int Id)
+    {
+        this.ID = Id;
+    }
+
+
 }
