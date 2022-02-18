84fd9388e12d5c8f00bc23eb8fc3581c32facc34
HayriOZTURK <you@example.com>
2022-02-18 15:18:39 +0300

    hocam yavas

--- DTO/PetDTO.cs
+++ DTO/PetDTO.cs
@@ -1,3 +1,18 @@
 public class PetDTO {
-    
+    public static List<PetDTO> PetList=new List<PetDTO>();
+    public int id { get; set; }
+    public string  name { get; set; }
+    public DateTime BirthDate { get; set; }
+    public Family Family { get; set; }
+    public Gender Gender { get; set; }
+    public string Genus { get; set; }
+        public int UserID { get; set; }
+    public string About { get; set; }
+
+
+public PetDTO(int id){
+
+    this.id=id;
+}
+
 }
