a6ec9b1091e7ecf5694f1461f43b215a6bd3638b
efbugart <efbugart@gmail.com>
2022-02-18 15:48:06 +0300

    petController edit

---
+++ Controllers/PetController.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PetCity.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PetController : ControllerBase
+{    
+
+
+    [HttpGet]
+    public List<PetDTO> Get()
+    {
+        return PetDTO.PetList;        
+    }
+
+ [HttpGet("{id}")]
+    public PetDTO GetUser(int id)
+    {
+        var pet = PetDTO.PetList.FirstOrDefault(x=>x.id == id);
+        return pet;       
+    }
+
+
+    [HttpPost]
+    public string Add(PetDTO pet){
+        PetDTO.PetList.Add(pet);
+        return "Ok";
+    }
+
+    [HttpDelete ("{id}")]
+    public void Delete(int id)
+    {
+        var deletePet = PetDTO.PetList.FirstOrDefault(x=>x.id ==id);
+        PetDTO.PetList.Remove(deletePet);
+    }
+
+
+    [HttpPut]
+    public PetDTO Put([FromBody] PetDTO petDTO)
+    {
+        var editedPet = PetDTO.PetList.FirstOrDefault(x=>x.id == petDTO.id);
+        editedPet.name = petDTO.name;
+        editedPet.About = petDTO.About;
+        editedPet.BirthDate = petDTO.BirthDate;
+        editedPet.Family = petDTO.Family;
+        editedPet.Gender = petDTO.Gender;
+        editedPet.Genus = petDTO.Genus;
+        editedPet.UserID = petDTO.UserID;
+
+        return editedPet;
+
+    }
+}
