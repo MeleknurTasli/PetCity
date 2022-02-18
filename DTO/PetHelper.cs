a4396a2c8d13031be4053cb9d7fc9296612e7e4c
Mustafa <mustafanck3@gmail.com>
2022-02-18 12:43:55 +0300

    PetHelper Refactoring

--- DTO/PetHelper.cs
+++
@@ -1,3 +0,0 @@
-public class PetHelper{
-    public int Id { get; set; }
-}
---
+++ DTO/PetHelperDTO.cs
@@ -0,0 +1,6 @@
+public class PetHelperDTO{
+    public int Id { get; set; }
+    public int PetId { get; set; }
+    public int UserId { get; set; }
+
+}
