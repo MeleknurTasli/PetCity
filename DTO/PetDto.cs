227fb8debf6b1ae9efb2a7bdf8c7b6515e5a6702
HayriOZTURK <you@example.com>
2022-02-18 12:59:09 +0300

    Add_PetDTO

--- DTO/PetDto.cs
+++ DTO/PetDto.cs
@@ -1,5 +1,15 @@
-public class PetDto{
-public int PetId { get; set; }
-public string PetName { get; set; }
-public int Age { get; set; }
+
+public class PetDTO{
+
+public int Id { get; set; }
+public string Name { get; set; }
+public DateTime BirthDate { get; set; }
+public Family Family { get; set; }
+public string Genus { get; set; }
+public int  UserID { get; set; }
+public string About { get; set; }
+public Gender Gender { get; set; }
+
+
+
 };
