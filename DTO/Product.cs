8103e8bb48e668a271a4002b2bb3db79e0ed5aa5
unknown <unsurenes@gmail.com>
2022-02-18 12:40:02 +0300

    properties are added

---
+++ DTO/Product.cs
@@ -0,0 +1,11 @@
+public class Product
+{
+    public int  ProductID { get; set; }
+    public string ProductName { get; set; }
+    public int CategoryID { get; set; }
+    public string Brand { get; set; }
+    public int Stock { get; set; }
+    public double Price { get; set; }
+    
+
+}
