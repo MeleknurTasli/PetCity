c1a6cb06ab550f717faa6c95ceedaf15551263e2
Fatih Murat Taştemel <fatihmurattastemel@gmail.com>
2022-02-21 16:46:50 +0300

    switch case deleted

--- Helper/MessageGenarator.cs
+++ Helper/MessageGenarator.cs
@@ -1,36 +1,21 @@
 public static class MessageGenarator
 {
+    public static readonly Dictionary<ResponseCodeEnum, string> TrResponseMessages
+        = new Dictionary<ResponseCodeEnum, string>
+    {
+    { ResponseCodeEnum.Success, "Ok" },
+    { ResponseCodeEnum.GetAllAccountOperationSuccess,"Ok" },
+     {ResponseCodeEnum.DuplicateAccountError,"Girilen e mail sistemde kayıtlı" },
+{ResponseCodeEnum.GetAccountByEmailOperationSuccess,"Girilen maile sahip account başarılı bir şekilde bulundu."},
+{ ResponseCodeEnum.GetAccountByEmailOperationFail,"Girilen maile sahip account bulunamadı"},
+
+
+    };
+
 
     public static string ResponseMessageGenarator(ResponseCodeEnum ResponseCode)
     {
-        switch (ResponseCode)
-        {
-
-            case ResponseCodeEnum.Success:
-            case ResponseCodeEnum.GetAllAccountOperationSuccess:
-                {
-                    return "Ok";
-                }
-
-            case ResponseCodeEnum.DuplicateAccountError:
-                {
-                    return "Girilen e mail sistemde kayıtlı";
-                }
-
-            case ResponseCodeEnum.GetAccountByEmailOperationSuccess:
-                {
-                    return "Girilen maile sahip account başarılı bir şekilde bulundu.";
-                }
-
-            case ResponseCodeEnum.GetAccountByEmailOperationFail:
-                {
-                    return "Girilen maile sahip account bulunamadı";
-                }
-            default:
-                {
-                    return "blablabla";
-                }
-        }
+        return TrResponseMessages[ResponseCode];
     }
 
 }
