f277a332a12c857829a562bb932448a82616a003
Mustafa <mustafanck3@gmail.com>
2022-02-24 10:44:29 +0300

    merge

--- Helper/MessageGenarator.cs
+++ Helper/MessageGenarator.cs
@@ -37,7 +37,6 @@ public static class MessageGenarator
     {ResponseCodeEnum.FindPetHelperByLatLongFail,"Koordinat bulunamadı."},
     {ResponseCodeEnum.PetHelperAddSuccess,"Ok"},
     {ResponseCodeEnum.PetHelperAddFail,"Aynı Id'de veri bulundu."},
-<<<<<<< HEAD
     {ResponseCodeEnum.PetHelperGetByIdSuccess, "Ok"},
     {ResponseCodeEnum.PetHelperGetByIdFail, "Verilen Id'ye ait PetHelper bulunamadı."},
     {ResponseCodeEnum.PetHelperUpdateSuccess, "Ok"},
@@ -46,10 +45,6 @@ public static class MessageGenarator
     {ResponseCodeEnum.GetPetHelperDeleteFail,"Silinecek Veri Bulunamadı"}
 
 
-=======
-    { ResponseCodeEnum.GetPetByGenusOperationSuccess,"Ok"},
-    { ResponseCodeEnum.GetPetByGenusOperationFail,"Girilen cinse sahip pet bulunamadı"}
->>>>>>> dev/main
 
     };
 
