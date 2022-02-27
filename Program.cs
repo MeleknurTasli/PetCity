5fb5911ca7ae7b120e149a201e5b1adbcd2e5089
ugurunsal <ugurnsal@gmail.com>
2022-02-27 13:43:45 +0300

    PetCityContext added to program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,9 +15,17 @@ var app = builder.Build();
   using (var context = new LibraryContext()) {
         context.Database.EnsureCreated();
     }
-    using (var context = new PetCityContext()) {
-        context.Database.EnsureCreated();
+
+    using(var context = new PetCityContext())
+    {  
+        if(context.Database.EnsureCreated())
+        {
+        var databaseCreator = context.Database.GetService<IServiceProvider>().GetService<IRelationalDatabaseCreator>();
+        if(databaseCreator is not null)
+        databaseCreator.CreateTables();
+        }
     }
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
