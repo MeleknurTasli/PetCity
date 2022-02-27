3e5f2862ced1d3bec232f5b2ce061193ceca1c9f
Mustafa <mustafanck3@gmail.com>
2022-02-27 10:09:58 +0300

    dbContext added

--- Model/Category.cs
+++ Model/Category.cs
@@ -1,3 +1,13 @@
 public class Category{
-    
+    public int CategoryId { get; set; }
+    public string? Name { get; set; } 
+    public virtual ICollection<Product>? Products { get; set; }
+
+        public override string ToString() {
+        var txt = new StringBuilder();
+        txt.AppendLine($"Id: {CategoryId}");
+        txt.AppendLine($"Category: {Name}");
+        
+        return txt.ToString();
+    }
 }
--- Model/Company.cs
+++ Model/Company.cs
@@ -1,3 +1,15 @@
-public class Company{
-    
+public class Company
+{
+    public int CompanyId { get; set; }
+    public string? Name { get; set; }
+    public virtual ICollection<Product>? Products { get; set; }
+
+    public override string ToString()
+    {
+        var txt = new StringBuilder();
+        txt.AppendLine($"Id: {CompanyId}");
+        txt.AppendLine($"Company: {Name}");
+
+        return txt.ToString();
+    }
 }
