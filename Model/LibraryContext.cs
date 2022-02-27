73a3564009fcf32af1b1ffa214213e00f83e138b
Mustafa <mustafanck3@gmail.com>
2022-02-27 10:13:56 +0300

    dbContext added

--- Model/LibraryContext.cs
+++ Model/LibraryContext.cs
@@ -3,8 +3,6 @@ public class LibraryContext : DbContext
 {
     public DbSet<Book>? Books { get; set; }
     public DbSet<Publisher>? Publishers { get; set; }
-    public DbSet<Product>? Products { get; set; }
-    public DbSet<Category>? Categories { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseMySQL("server=localhost;database=sahabt;user=root;port=3306;password=toortoor");
