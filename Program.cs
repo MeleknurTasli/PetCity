f984f7f89888537b041a140970563f837f185ca4
ugurunsal <ugurnsal@gmail.com>
2022-02-28 16:33:40 +0300

    product service added

--- Program.cs
+++ Program.cs
@@ -10,7 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<PetCityContext>();
+builder.Services.AddScoped<IProductRepository,ProductRepository>();
+builder.Services.AddScoped<IProductService,ProductService>();
 // builder.Services.AddScoped<IAccountService,AccountService>();
 var app = builder.Build();
 // Configure the HTTP request pipeline.
