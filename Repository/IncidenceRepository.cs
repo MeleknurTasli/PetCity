f9af080b96c1023f9aa473a0f70f49f7e03dd6a9
MeleknurTasli <melekselcen@gmail.com>
2022-02-28 16:11:25 +0300

    Incidence

---
+++ Repository/IncidenceRepository.cs
@@ -0,0 +1,9 @@
+public class IncidenceRepository
+{
+    PetCityContext _petCityContext;
+
+    public IncidenceRepository(PetCityContext petCityContext)
+    {
+        _petCityContext=petCityContext;
+    }
+}
