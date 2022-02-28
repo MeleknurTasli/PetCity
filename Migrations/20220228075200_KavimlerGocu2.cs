630e2434a5e8006cbf0f6a48353ed009f49d17ce
Fatih Murat Taştemel <fatihmurattastemel@gmail.com>
2022-02-28 10:57:53 +0300

    backup commit

---
+++ Migrations/20220228075200_KavimlerGocu2.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetCity.Migrations
+{
+    public partial class KavimlerGocu2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProductId",
+                table: "Products",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "CompanyId",
+                table: "Companies",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoryId",
+                table: "Categories",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "BrandId",
+                table: "Brands",
+                newName: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Products",
+                newName: "ProductId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Companies",
+                newName: "CompanyId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Categories",
+                newName: "CategoryId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Brands",
+                newName: "BrandId");
+        }
+    }
+}
