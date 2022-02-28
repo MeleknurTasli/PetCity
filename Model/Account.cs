542cb84cf07c4c258416b6fe8fac1773beabde2c
Fatih Murat Taştemel <fatihmurattastemel@gmail.com>
2022-02-28 12:50:27 +0300

    backup commit

--- Model/Account.cs
+++ Model/Account.cs
@@ -1,19 +1,22 @@
-public class Account {
+public class Account
+{
     public int Id { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
     public bool IsBlocked { get; set; }
     public bool Visibility { get; set; }
+    public virtual ICollection<Role> Role { get; set; }
+
     public Account()
     {
-        
+
     }
-    public Account(int id,string email,string password,bool isblocked,bool visibility)
+    public Account(int id, string email, string password, bool isblocked, bool visibility)
     {
-        this.Id=id;
-        this.Email=email;
-        this.Password=password;
-        this.IsBlocked=isblocked;
-        this.Visibility=visibility;
+        this.Id = id;
+        this.Email = email;
+        this.Password = password;
+        this.IsBlocked = isblocked;
+        this.Visibility = visibility;
     }
 }
--- Model/PetCityContext.cs
+++ Model/PetCityContext.cs
@@ -43,29 +43,25 @@ public class PetCityContext : DbContext
             .WithMany(c => c!.Products);
         });
 
-         modelBuilder.Entity<Account>(entity =>
-        {
-            entity.HasKey(e => e.Id);
-            entity.Property(e => e.Email).IsRequired();
-            entity.Property(e => e.Password).IsRequired();
-            entity.Property(e => e.IsBlocked).IsRequired();
-            entity.Property(e => e.Visibility).IsRequired();
-        });
-         modelBuilder.Entity<Role>(entity =>
-        {
-            entity.HasKey(e => e.Id);
-            entity.Property(e => e.Name).IsRequired();
-        });
-         modelBuilder.Entity<AccountRole>(entity =>
-        {
-            entity.HasKey(e => e.Id);
-            entity.Property(e => e.account).IsRequired();
-             entity.Property(e => e.role).IsRequired();
-             entity.HasOne(e => e.account);
-         
-          
-            
-        });
+        modelBuilder.Entity<Account>(entity =>
+       {
+           entity.HasKey(e => e.Id);
+           entity.Property(e => e.Email).IsRequired();
+           entity.Property(e => e.Password).IsRequired();
+           entity.Property(e => e.IsBlocked).IsRequired();
+           entity.Property(e => e.Visibility).IsRequired();
+           entity.HasMany(e => e.Role).WithMany(e => e.Account).UsingEntity(j => j.ToTable("Account_Role"));
+
+       });
+
+
+
+        modelBuilder.Entity<Role>(entity =>
+       {
+           entity.HasKey(e => e.Id);
+           entity.Property(e => e.Name).IsRequired();
+       });
+
 
         modelBuilder.Entity<Category>().HasData(
             new Category
@@ -130,45 +126,33 @@ public class PetCityContext : DbContext
             {
                 Id = 1,
                 Email = "meryem.dogan@sahabt.com",
-                Password= "123123",
-                IsBlocked=true,
-                Visibility=true
+                Password = "123123",
+                IsBlocked = true,
+                Visibility = true
             },
             new Account
             {
                 Id = 2,
                 Email = "galipcan.karaaslan@sahabt.com",
-                Password= "555555",
-                IsBlocked=true,
-                Visibility=true
+                Password = "555555",
+                IsBlocked = true,
+                Visibility = true
             }
         );
 
-         modelBuilder.Entity<Role>().HasData(
-            new Role
-            {
-                Id = 1,
-                Name="admin"
-                
-            },
-            new Role
-            {
-                Id = 2,
-                Name="moderator"
-                
-            }
-        );
+        modelBuilder.Entity<Role>().HasData(
+           new Role
+           {
+               Id = 1,
+               Name = "admin"
 
-         modelBuilder.Entity<AccountRole>().HasData(
-            new AccountRole
-            {
-                Id = 1,       
-            },
-            new AccountRole
-            {
-                Id = 2,             
-            }
-        );
-        
+           },
+           new Role
+           {
+               Id = 2,
+               Name = "moderator"
+
+           }
+       );
     }
 }
