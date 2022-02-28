16b06fd4757fe4c6ddbfe0bdd0ed480e2be3c770
ugurunsal <ugurnsal@gmail.com>
2022-02-28 13:52:34 +0300

    Product Repo implemented

--- Repository/ProductRepository.cs
+++ Repository/ProductRepository.cs
@@ -1,3 +1,68 @@
-public class ProductRepository{
-    
+public class ProductRepository : IProductRepository
+{
+    private readonly PetCityContext _context;
+
+    public ProductRepository(PetCityContext context)
+    {
+        _context=context;
+    }
+    public async Task<Product> AddProduct(Product product)
+    {
+        using(_context)
+        {
+            await _context.AddAsync(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+    }
+
+    public async Task DeleteProduct(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<List<Product>> GetAllProduct()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<List<Product>> GetAllProductByBrandId(int brandId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<List<Product>> GetAllProductByCategoryId(int categoryId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<List<Product>> GetAllProductByName(string productName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<List<Product>> GetAllProductByPriceASC(int price)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<List<Product>> GetAllProductByPriceDESC(int price)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<List<Product>> GetAllProductByPriceRange(int minPrice, int maxPrice)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Product> GetByProductId(int productId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Product> UpdateProduct(int id, Product product)
+    {
+        throw new NotImplementedException();
+    }
 }
