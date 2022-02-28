86710c9f23add68c002f05034c2e84a5f05d74aa
abidinyarata <abidinyarata@windowslive.com>
2022-02-28 11:49:34 +0300

    merge conflict

---
+++ Migrations/20220228084811_deneme.cs
@@ -0,0 +1,170 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetCity.Migrations
+{
+    public partial class deneme : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Brands",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Brands", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    TestPropString = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    TestPropInt = table.Column<int>(type: "int", nullable: true),
+                    UnitPrice = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    UnitsInStock = table.Column<short>(type: "smallint", nullable: false),
+                    QuantityPerUnit = table.Column<short>(type: "smallint", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    BrandId = table.Column<int>(type: "int", nullable: false),
+                    CompanyId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Brands_BrandId",
+                        column: x => x.BrandId,
+                        principalTable: "Brands",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Products_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Products_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Companies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Brands",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Pro Plan" },
+                    { 2, "Pro Line" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Cat Food" },
+                    { 2, "Dog Food" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "BlaBla Sirketi" },
+                    { 2, "BlaBlaBla Sirketi" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "BrandId", "CategoryId", "CompanyId", "Name", "QuantityPerUnit", "TestPropInt", "TestPropString", "UnitPrice", "UnitsInStock" },
+                values: new object[] { 1, 1, 1, 1, "Pro Plan Active", (short)0, null, null, 15m, (short)10 });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "BrandId", "CategoryId", "CompanyId", "Name", "QuantityPerUnit", "TestPropInt", "TestPropString", "UnitPrice", "UnitsInStock" },
+                values: new object[] { 2, 2, 2, 2, "Pro Line Active", (short)0, null, null, 10m, (short)15 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_BrandId",
+                table: "Products",
+                column: "BrandId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CompanyId",
+                table: "Products",
+                column: "CompanyId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Brands");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Companies");
+        }
+    }
+}
--- Model/PetCityContext.cs
+++ Model/PetCityContext.cs
@@ -4,14 +4,12 @@ public class PetCityContext : DbContext
     public DbSet<Category>? Categories { get; set; }
     public DbSet<Company>? Companies { get; set; }
     public DbSet<Brand>? Brands { get; set; }
-    public DbSet<Pet>? Pets { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         var serverVersion = new MySqlServerVersion(new Version(8, 0, 28));
 
         optionsBuilder.UseMySql("server=localhost;database=sahabt;user=root;port=3306;password=toortoor", serverVersion);
-
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -90,133 +88,6 @@ public class PetCityContext : DbContext
                 CategoryId = 1,
                 CompanyId = 1
             },
-            new Product
-            {
-                Id = 1,
-                Name = "Pro Line Active",
-                UnitPrice = 10,
-                UnitsInStock= 15,
-                BrandId =2,
-                CategoryId=2,
-                CompanyId=2
-            }
-        );
-
-        modelBuilder.Entity<Pet>(entity =>
-        {
-            entity.HasKey(e => e.Id);
-            entity.Property(e => e.Name).IsRequired().HasMaxLength(50);
-            entity.Property(e => e.PetImageUrl).IsRequired();
-            entity.Property(e=> e.HealthStatus).IsRequired();
-            
-        });
-
-        modelBuilder.Entity<Pet>().HasData(
-            new Pet{
-                Id = 1,
-                Name = "karabaş",
-                Age = 2,
-                Gender = PetGender.Male,
-                HealthStatus = HealthStatus.Green,
-                PetImageUrl = "avsba12323",
-                Species = new PetSpecies(){
-                    Id=1,
-                    Name = "Köpek"
-                },
-                SubSpecies = new PetSubSpecies(){
-                    Id = 1,
-                    Name = "Kangal"
-                }
-            },
-            new Pet{
-                Id = 2,
-                Name = "minnoş",
-                Age = 5,
-                 Gender = PetGender.Female,
-                 HealthStatus = HealthStatus.Yellow,
-                PetImageUrl = "avsba12343",
-                  Species = new PetSpecies(){
-                     Id=2,
-                     Name = "Kedi"
-                 },
-                 SubSpecies = new PetSubSpecies(){
-                    Id = 1,
-                     Name = "Tekir"
-
-                 }
-
-
-            }
-        );
-
-        modelBuilder.Entity<Category>(entity =>
-        {
-            entity.HasKey(e => e.Id);
-            entity.Property(e => e.Name).IsRequired();
-        });
-        modelBuilder.Entity<Company>(entity =>
-        {
-            entity.HasKey(e => e.Id);
-            entity.Property(e => e.Name).IsRequired();
-        });
-        modelBuilder.Entity<Product>(entity =>
-        {
-            entity.HasKey(e => e.Id);
-            entity.Property(e => e.Name).IsRequired();
-            entity.HasOne(p => p.Category)
-            .WithMany(c => c!.Products);
-            entity.HasOne(b => b.Brand)
-            .WithMany(c => c!.Products);
-            entity.HasOne(c => c.Company)
-            .WithMany(c => c!.Products);
-        });
-        modelBuilder.Entity<Category>().HasData(
-            new Category
-            {
-                Id = 1,
-                Name = "Cat Food"
-                },
-            new Category
-            {
-                Id = 2,
-                Name = "Dog Food"
-                }
-        );
-        modelBuilder.Entity<Company>().HasData(
-            new Company
-            {
-                Id = 1,
-                Name = "BlaBla Sirketi"
-                },
-            new Company
-            {
-                Id = 2,
-                Name = "BlaBlaBla Sirketi"
-                }
-        );
-            modelBuilder.Entity<Brand>().HasData(
-            new Brand
-            {
-                Id = 1,
-                Name = "Pro Plan"
-                },
-            new Brand
-            {
-                Id = 2,
-                Name = "Pro Line"
-                }
-        );
-        modelBuilder.Entity<Product>().HasData(
-            new Product
-            {
-                Id = 1,
-                Name = "Pro Plan Active",
-                UnitPrice = 15,
-                UnitsInStock= 10,
-                BrandId =1,
-                CategoryId=1,
-                CompanyId=1
-                },
             new Product
             {
                 Id = 2,
