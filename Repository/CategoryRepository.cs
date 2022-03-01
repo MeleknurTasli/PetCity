997f3035f7a1aa496f8c50d3e5b8873640518dbe
ugurunsal <ugurnsal@gmail.com>
2022-03-01 12:00:46 +0300

    implement categoryrepo interface

--- Repository/CategoryRepository.cs
+++ Repository/CategoryRepository.cs
@@ -1,4 +1,32 @@
 public class CategoryRepository : ICategoryRepository
 {
+    public Task<Category> AddCategory(Category category)
+    {
+        throw new NotImplementedException();
+    }
 
+    public Task DeleteCategory(Category category)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Category>> GetAllCategory()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Category>> GetAllCategoryByName(string categoryName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Category> GetByCategoryId(int categoryId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Category> UpdateCategory(int id, Category category)
+    {
+        throw new NotImplementedException();
+    }
 }
